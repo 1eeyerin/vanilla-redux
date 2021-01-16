65fee37a859d49536b05fb99786ef066dbd14bee
ye-r1 <yerin@iyelin-ui-MacBookPro.local>
2021-01-16 16:02:57 +0900

    #4.2 Redux Toolkit, createReducer

--- src/store.js
+++ src/store.js
@@ -1,34 +1,37 @@
 import {createStore} from "redux";
+import {createAction, createReducer} from "@reduxjs/toolkit";
 
 //define action name
-const ADD = "ADD";
-const DELETE = "DELETE";
+/*const ADD = "ADD";
 
 //action creator
-const addToDo = (text) => {
+/*const addToDo = (text) => {
     return {
         type: ADD,
         text
     }
-}
-const deleteToDo = (id) => {
-    return {
-        type: DELETE,
-        id
-    }
-}
+}*/
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 //reducer
-const reducer = (state = [], action) => {
+/*const reducer = (state = [], action) => {
     switch (action.type){
-        case ADD:
-            return [{text:action.text, id:Date.now()}, ...state];
-        case DELETE:
-            return state.filter(toDo => toDo.id !== action.id);
+        case addToDo.type:
+            return [{text:action.payload, id:Date.now()}, ...state];
+        case deleteToDo.type:
+            return state.filter(toDo => toDo.id !== action.payload);
         default:
             return state;
     }
-}
+}*/
+const reducer = createReducer([], {
+    [addToDo]: (state, action) => {
+        state.push({text:action.payload, id:Date.now()});
+    },
+    [deleteToDo]: (state, action) =>
+        state.filter(toDo => toDo.id !== action.payload)
+ })
 
 //export action creators
 export const actionCreators = {
